2c0d8aa8e84308e22d48f6197e4510cbb9ceed0c
Fabian_Trojer <trojer@geoville.com>
2023-04-21 10:15:06 +0200

    Eliminated hardcoded paths & added explanation

--- create_report_tif_to_csv.py
+++ create_report_tif_to_csv.py
@@ -1,13 +1,30 @@
+"""
+
+@author: fabian trojer & tashi freyer | Geoville
+
+Description: Open and read two .tif files and generate .csv file containing comparison statistics such as
+-number of pixels per land cover class
+-number of thematically (un)changed pixels
+-fraction per land cover class
+-fraction change per land cover class
+-accuracy (assuming that first .tif file serves as ground truth)
+
+
+             Necessary Parameters:
+                 - path to .tif file 1 (e.g. r"C:\gv\test_area_raster.tif")
+                 - path to .tif file 2 (e.g. r"C:\gv\test_area_raster.tif")
+                 - output folder where generated .csv file is meant to be stored (r"C:\gv\BB")
+
+"""
 import csv
 import pandas as pd
-import geopandas as gpd
 import rasterio
 import numpy as np
-import os
 from pathlib import Path
 
 
 def create_report_tif_to_csv(tif_file_2018, tif_file_2021, output_folder):
+
     # OPEN TIF FILES -----------------------------------------------------
     dataset2018 = rasterio.open(tif_file_2018)
     dataset2021 = rasterio.open(tif_file_2021)
@@ -20,7 +37,7 @@ def create_report_tif_to_csv(tif_file_2018, tif_file_2021, output_folder):
     array2021 = dataset2021.read(1)
     print("The shape of the 2021 file is:", array2021.shape)
 
-    # NUMBER OF CLASSES
+    # GET NUMBER OF CLASSES
     num_class = list(range(1, 12))
     print(num_class)
 
@@ -30,7 +47,7 @@ def create_report_tif_to_csv(tif_file_2018, tif_file_2021, output_folder):
         1: "2021"
     }
 
-    # DEFINE THE LAND COVER CLASSES AND THEIR NAMES
+    # DEFINE LAND COVER CLASSES AND THEIR NAMES
     land_cover_classes = {
         0: "Sealed",
         1: "Woody – needle leaved trees",
@@ -91,18 +108,25 @@ def create_report_tif_to_csv(tif_file_2018, tif_file_2021, output_folder):
 
     # OPEN A NEW CSV FILE FOR WRITING--------------------------------------
     with open(outpath, 'w', newline='') as f:
-        # CREATE A CSV WRITER OBJECT-----------------------------------------
+
+        # CREATE A CSV WRITER OBJECT
         writer = csv.writer(f)
 
         # ADD CLCBB STATISTICS
-        writer.writerow([f"CLCBB Validation Report\n\n"])
-        writer.writerow([f"CLCBB\n\n"])
+        writer.writerow([f"CLCBB Validation Report"])
+        writer.writerow([f"CLCBB"])
+
+        # CREATE AN EMPTY ROW
+        empty_row = []
+
+        # WRITE THE EMPTY ROW TO THE CSV FILE
+        writer.writerows([empty_row])
 
         df = pd.DataFrame({"2018": count2018,
                            "2021": count2021,
                            "difference_18_21": difference_18_21,
                            "pixels_no_change": pixels_no_change,
-                           "accuracy": accuracy}).T
+                           "accuracy/class": accuracy}).T
 
         df = df.rename(columns=land_cover_classes)
         print(df)
@@ -111,15 +135,21 @@ def create_report_tif_to_csv(tif_file_2018, tif_file_2021, output_folder):
         # ADD OVERALL ACCURACY
         writer.writerow(['Overall accuracy:', '{:.2f}'.format(overall_accuracy)])
 
-        # Calculate the total number of pixels for all land cover classes
+        # CALCULATE THE TOTAL NUMBER OF PIXELS FOR ALL LAND COVER CLASSES
         total_pixels_2018 = sum(count2018) + sum(difference_18_21)
         total_pixels_2021 = sum(count2021) + sum(difference_18_21)
 
-        # Generate the statistics for each land cover class
+        # GENERATE STATISTICS FOR EACH LAND COVER CLASS
         for index, land_cover in land_cover_classes.items():
+            # CREATE AN EMPTY ROW
+            separation_row = [f"-----------------------"]
+
+            # WRITE THE EMPTY ROW TO THE CSV FILE
+            writer.writerows([separation_row])
+
             writer.writerow([f"{index + 1} {land_cover} statistics\n\n"])
 
-            # Calculate the fraction of pixels for each land cover class
+            # CALCULATE FRACTION OF PIXELS FOR EACH LAND COVER CLASS
             if total_pixels_2018 != 0:
                 fraction_2018 = (count2018[index] + difference_18_21[index]) / total_pixels_2018 * 100
             else:
@@ -137,10 +167,10 @@ def create_report_tif_to_csv(tif_file_2018, tif_file_2021, output_folder):
                 "2021": count2021[index],
                 "difference_18_21": difference_18_21[index],
                 "pixels_no_change": pixels_no_change[index],
-                "accuracy": accuracy[index],
-                "fraction_2018": fraction_2018,
-                "fraction_2021": fraction_2021,
-                "fraction_change": fraction_change,
+                "class_accuracy": accuracy[index],
+                "fraction_2018(%)": fraction_2018,
+                "fraction_2021(%)": fraction_2021,
+                "fraction_change(%)": fraction_change,
             }, index=['Total pixels']).T
 
             df.to_csv(f, sep=",")
